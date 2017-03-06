8804c1d32aae5328b8a5914bf3ad5d17fa9694de
sfreeman <Steffanie.Freeman@d2l.com>
2017-03-06 16:50:49 -0500

    Rename is_mfa_enabled to mfa_enabled
    Signed-off-by: sfreeman <Steffanie.Freeman@d2l.com>

--- test/unit/resources/iam_test.rb
+++ test/unit/resources/iam_test.rb
@@ -19,12 +19,12 @@ Username = "test"
 
   def test_that_MFA_enable_returns_true_if_MFA_Enabled
     @mockUser.expect :mfa_devices, ["test"]
-    assert Iam.new(Username, @mockConn).is_mfa_enabled?
+    assert Iam.new(Username, @mockConn).mfa_enabled?
   end
 
   def test_that_MFA_enable_returns_false_if_MFA_is_not_Enabled
     @mockUser.expect :mfa_devices, []
-    assert !Iam.new(Username, @mockConn).is_mfa_enabled?
+    assert !Iam.new(Username, @mockConn).mfa_enabled?
   end
 
   def test_that_console_Password_returns_true_if_console_Password_has_been_set
