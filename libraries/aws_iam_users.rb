a4efd4c90310f8b5c8695eab171b8b829694c0bc
Jeffrey Lyons <Jeffrey.Lyons@D2L.com>
2017-03-16 15:28:50 -0400

    Adding list_users method to back filter table
    Signed-off-by: Jeffrey Lyons <Jeffrey.Lyons@D2L.com>

--- libraries/aws_iam_users.rb
+++ libraries/aws_iam_users.rb
@@ -1,5 +1,8 @@
 # author: Alex Bedley
 # author: Steffanie Freeman
+# author: Jeffrey Lyons
+# author: Simon Varlow
+
 class AwsIamUsers < Inspec.resource(1)
   name 'aws_iam_users'
   desc 'Verifies settings for AWS IAM users'
@@ -9,5 +12,10 @@ class AwsIamUsers < Inspec.resource(1)
     end
   "
   def initialize(conn = AWSConnection.new)
+    @iam_resource = conn.iam_resource
+  end
+  
+  def list_users()
+    @iam_resource.users
   end
 end
