7f48f12d47fed1e7b7786ab66e72ed33b71e7f9c
Simon Varlow <simon.varlow@desire2learn.com>
2017-04-04 14:46:28 -0400

    Added in MFA device tests
    Signed-off-by: Simon Varlow <simon.varlow@desire2learn.com>

--- test/unit/resources/aws_iam_user_provider_test.rb
+++ test/unit/resources/aws_iam_user_provider_test.rb
@@ -19,4 +19,18 @@ Username = "test"
    	@mock_iam_reasource.expect :user, {}, [Username]
    	assert !@user_provider.get_user(Username).nil?
    end
+
+   def test_that_MFA_enable_returns_true_if_MFA_Enabled?
+    @mock_user = Minitest::Mock.new
+    @mock_user.expect :mfa_devices, [true]
+    @mock_iam_reasource.expect :user, @mock_user, [Username]
+    assert !@user_provider.has_mfa_enabled(Username)
+   end 
+
+   def test_that_MFA_enable_returns_false_if_MFA_Enabled?
+    @mock_user = Minitest::Mock.new
+    #@mock_user.expect :mfa_devices, [false]
+    @mock_iam_reasource.expect :user, @mock_user, [Username]
+    assert !@user_provider.has_mfa_enabled(Username)
+   end
 end
