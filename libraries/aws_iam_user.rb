92ee88c13bbadabf22da30b08f35bf4f270c9ab6
sfreeman <Steffanie.Freeman@d2l.com>
2017-05-19 15:50:11 -0400

    Add aws_iam_users
    Signed-off-by: sfreeman <Steffanie.Freeman@d2l.com>

--- libraries/aws_iam_user.rb
+++ libraries/aws_iam_user.rb
@@ -5,14 +5,14 @@ class AwsIamUser < Inspec.resource(1)
   name 'aws_iam_user'
   desc 'Verifies settings for AWS IAM user'
   example "
-    describe aws_iam_user('test_user_name') do
+    describe aws_iam_user(name: 'test_user_name') do
       its('has_mfa_enabled?') { should be false }
       its('has_console_password?') { should be true }
     end
   "
-  def initialize(name, aws_user_provider = AwsIam::UserProvider.new)
-    @name = name
-    @user = aws_user_provider.user(name)
+  def initialize(opts, aws_user_provider = AwsIam::UserProvider.new)
+    @user = opts[:user]
+    @user = aws_user_provider.user(opts[:name]) if @user.nil?
   end
 
   def has_mfa_enabled?
