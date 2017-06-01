652929a850993124ccc347a3be5216210ca1543c
aduric <adnan.duric@d2l.com>
2017-06-01 14:56:14 -0400

    Mocks the user factory

--- test/unit/resources/aws_iam_users_test.rb
+++ test/unit/resources/aws_iam_users_test.rb
@@ -9,6 +9,7 @@ class AwsIamUsersTest < Minitest::Test
 
   def setup
     @mock_user_provider = Minitest::Mock.new
+    @mock_user_factory = Minitest::Mock.new
   end
 
   def test_mfa_enabled_returns_true_for_all_users_if_mfa_enabled
@@ -18,6 +19,9 @@ class AwsIamUsersTest < Minitest::Test
 
     @mock_user_provider.expect :list_users, user_list
     @mock_user_provider.expect :nil?, false
+    @mock_user_factory.expect :create_user, AwsIamUser.new(user: user_list[0])
+    @mock_user_factory.expect :create_user, AwsIamUser.new(user: user_list[1])
+    @mock_user_factory.expect :create_user, AwsIamUser.new(user: user_list[2])
     user_collection = AwsIamUsers.new(@mock_user_provider)
     user_collection.users.each do |user| 
       assert user.has_mfa_enabled?
