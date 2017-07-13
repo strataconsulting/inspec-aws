c852a52db1343814132179f88add948699218f16
sfreeman <Steffanie.Freeman@d2l.com>
2017-07-13 16:35:34 -0400

    aws_iam_user uses lazy loading
    Signed-off-by: sfreeman <Steffanie.Freeman@d2l.com>

--- test/unit/resources/aws_iam_user_test.rb
+++ test/unit/resources/aws_iam_user_test.rb
@@ -25,11 +25,13 @@ class AwsIamUserTest < Minitest::Test
 
   def test_that_console_Password_returns_true_if_console_Password_has_been_set
     @mock_user_provider.expect :user, {has_console_password?: true}, [Username]
+    @mock_user_provider.expect :has_console_password?, true, [Object]
     assert AwsIamUser.new({name: Username}, @mock_user_provider).has_console_password?
   end
 
   def test_that_console_Password_returns_false_if_console_Password_has_not_been_set
     @mock_user_provider.expect :user, {has_console_password?: false}, [Username]
+    @mock_user_provider.expect :has_console_password?, false, [Object]
     refute AwsIamUser.new({name: Username}, @mock_user_provider).has_console_password?
   end
 
@@ -39,6 +41,7 @@ class AwsIamUserTest < Minitest::Test
     mock_access_key_factory = Minitest::Mock.new
 
     @mock_user_provider.expect :user, {access_keys: [stub_aws_access_key]}, [Username]
+    @mock_user_provider.expect :access_keys, [stub_aws_access_key], [Object]
     mock_access_key_factory.expect :create_access_key, stub_access_key_resource, [stub_aws_access_key]
 
     assert_equal(stub_access_key_resource,
