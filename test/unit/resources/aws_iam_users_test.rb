db3e2e875283e6f7fd4dd0b65749af66101064b2
sfreeman <Steffanie.Freeman@d2l.com>
2017-07-20 15:22:27 -0400

    Disassociates convert call from list_users
    Signed-off-by: sfreeman <Steffanie.Freeman@d2l.com>

--- test/unit/resources/aws_iam_users_test.rb
+++ test/unit/resources/aws_iam_users_test.rb
@@ -6,6 +6,7 @@ require 'helper'
 require 'aws_iam_users'
 
 class AwsIamUsersTest < Minitest::Test
+  Username = "test" 
 
   def setup
     @mock_user_factory = Minitest::Mock.new
@@ -18,14 +19,28 @@ class AwsIamUsersTest < Minitest::Test
   end
 
   def test_users_empty_list_user_provider_returns_empty_list
-    cut = AwsIamUsers.new(create_mock_user_provider, @mock_user_factory)
+    mock_user_provider = Minitest::Mock.new
+    mock_user_provider.expect :list_users, []
+    mock_user_provider.expect :nil?, false
+    cut = AwsIamUsers.new(mock_user_provider, @mock_user_factory)
 
     assert_equal(cut.users,[])
   end
 
   def test_users_returns_true_for_all_users_if_mfa_enabled
+    mock_user_provider = Minitest::Mock.new
+    mock_user_provider.expect :list_users, [Username, Username]
+    mock_user_provider.expect :nil?, false
+    mock_user_provider.expect :convert, create_mock_user
+    mock_user_provider.expect :convert, create_mock_user
+    mock_user_provider.expect :has_mfa_enabled?, true
+    mock_user_provider.expect :has_mfa_enabled?, true
+    @mock_user_factory.expect :create_user, AwsIamUser.new({user: Username}, mock_user_provider, @mock_user_factory)
+    @mock_user_factory.expect :create_user, AwsIamUser.new({user: Username}, mock_user_provider, @mock_user_factory)
+
+
     cut = AwsIamUsers.new(
-      create_mock_user_provider(create_mock_users([true, true])),
+      mock_user_provider,
       @mock_user_factory
     )
 
@@ -33,7 +48,7 @@ class AwsIamUsersTest < Minitest::Test
       assert user.has_mfa_enabled?
     end
   end
-
+=begin
   [
     {
       name: 'test_where_returns_no_matching_rows',
@@ -67,17 +82,18 @@ class AwsIamUsersTest < Minitest::Test
 
     mock_user_provider.expect :list_users, user_list
     mock_user_provider.expect :nil?, false
-
+    mock_user_provider.expect :convert, 
     mock_user_provider
   end
 
   def create_mock_users(has_mfa_enableds = [])
     has_mfa_enableds.map { |x| create_mock_user(x) }
   end
-
+=end
   def create_mock_user(has_mfa_enabled = true)
     {
       has_mfa_enabled?: has_mfa_enabled
     }
   end
+
 end
